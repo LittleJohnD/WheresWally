a852900ddf2d73fa18968027c9907a75aa652856
John Dyer <jbdyer@gmail.com>
2015-01-05 15:20:04 +0000

    Step 2a complete

--- WheresWally/ViewController.m
+++ WheresWally/ViewController.m
@@ -16,18 +16,42 @@
 - (IBAction)nameAdd:(id)sender;
 - (IBAction)showLast:(id)sender;
 - (IBAction)showNext:(id)sender;
+
 @end
 
 @implementation ViewController
+//localivars
+NSMutableArray *listOfNames;
+int nameIndex;
+@synthesize lastButton;
+@synthesize nextButton;
+@synthesize nameEntry;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //Allocate and initialise listofNames as an empty list
+    listOfNames = [NSMutableArray new];
+    //Make nameIndex to be zero
+    nameIndex = 0;
+    //Hide both of the showNext/showLast buttons
+    
+    
+    lastButton.hidden=true;
+    nextButton.hidden=true;
+    //make nameEntry.delegate = self;
+    nameEntry.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL) textFieldShouldReturn: (UITextField *) textField {
+    [textField resignFirstResponder];
+    return YES;
 }
 
 - (IBAction)nameAdd:(id)sender {
@@ -38,3 +62,4 @@
 - (IBAction)showNext:(id)sender {
 }
 @end
+

21ed38009cf4ad7e96f2bd81c380e3604d93be24
John Dyer <jbdyer@gmail.com>
2015-01-05 15:46:19 +0000

    Tidy up and step 2b complete

--- WheresWally/ViewController.m
+++ WheresWally/ViewController.m
@@ -20,28 +20,24 @@
 @end
 
 @implementation ViewController
-//localivars
+//local ivars
 NSMutableArray *listOfNames;
 int nameIndex;
-@synthesize lastButton;
-@synthesize nextButton;
-@synthesize nameEntry;
+//@synthesize lastButton;
+//@synthesize nextButton;
+//@synthesize nameEntry;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
     //Allocate and initialise listofNames as an empty list
-    listOfNames = [NSMutableArray new];
+    listOfNames = [[NSMutableArray alloc] init];
     //Make nameIndex to be zero
     nameIndex = 0;
     //Hide both of the showNext/showLast buttons
-    
-    
-    lastButton.hidden=true;
-    nextButton.hidden=true;
+    self.lastButton.hidden=true;
+    self.nextButton.hidden=true;
     //make nameEntry.delegate = self;
-    nameEntry.delegate = self;
+    self.nameEntry.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -53,10 +49,27 @@ int nameIndex;
     [textField resignFirstResponder];
     return YES;
 }
-
 - (IBAction)nameAdd:(id)sender {
+    //if the textField is not empty, add the name to the list of names
+    if ([self.nameEntry.text length] == 0){
+        [listOfNames addObject:self.nameEntry];
+    }
+    //if the list of names is now 1 name long, then put the name in the label and make nameIndex = 1
+    if ([listOfNames count] > 0){
+        self.shownName.text = [listOfNames objectAtIndex:0];
+        nameIndex = 1;
+    }
+    //else if there are now more names in the list than nameIndex, then show the "Next" button.
+    if ([listOfNames count] > 1) {
+        self.nextButton.hidden=FALSE;
+    }
+    //if the new name is "Wally" then run the following alert code:
+    UIAlertView *gotWally = [[UIAlertView alloc] initWithTitle:@"Found Wally" message:@"There he is!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    [gotWally show];
+    //Clear the name from the text field.
+    self.nameEntry.text = nil;
+    
 }
-
 - (IBAction)showLast:(id)sender {
 }
 - (IBAction)showNext:(id)sender {
